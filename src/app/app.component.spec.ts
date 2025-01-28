4a42f5fbff679e06f18f67848fd1ae75760717d5
Marianne Mytting <mariamy@live.no>
2025-01-28 10:04:41 +0100

    TEST: Jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'angular-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-app');
+    expect(app.title).toBe('angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-app'
+    expect(compiled.querySelector('h1')?.textContent).toMatch(
+      /Hello, angular-app/
     );
   });
 });
