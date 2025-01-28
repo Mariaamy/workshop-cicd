e8e5772b861d091434085f639e0f7a029944bca2
Marianne Mytting <mariamy@live.no>
2025-01-28 09:54:03 +0100

    TEST: Basic ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,15 +6,16 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: globals.browser },
+  rules: {semi: "warn"}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    extends: ["eslint:recommended", "plugin:prettier/recommended"],
-    rules: {
-      quotes: ["error", "single"],
-      "no-console": "error",
-      curly: ["error", "all"]
-    }
-  }
+  // {
+  //   extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  //   rules: {
+  //     quotes: ["error", "single"],
+  //     "no-console": "error",
+  //     curly: ["error", "all"]
+  //   }
+  // }
 ];
